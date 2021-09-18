60a4b517fb11fe3240b98896eb17c01863748150
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-18 15:55:37 +0100

    cleanup :broom:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
